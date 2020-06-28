4d3b44e1262566f97cbb3399ff42831f93159e0a
oromanyuk <65210841+oromanyuk@users.noreply.github.com>
2020-06-28 16:09:55 +0500

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class calc
+    {
+        public static void Kvurav(double a, double b, double c)
+        {
+            double d = b * b - 2 * a * c;
+            if (d >= 0)
+            {
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("x1 = " + x1);
+                Console.WriteLine("x2 = " + x2);
+            }
+            else
+            {
+                Console.WriteLine("Корней нет!");
+            }
+        }
+    }
+    class calc2 : calc
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calc2.Kvurav(2, 4, 5);
+        }
+    }
+}
